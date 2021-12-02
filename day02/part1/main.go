5b60fee6d8fe310fa5b745d687bb601e6aa22b71
sam wood <samwood668@gmail.com>
2021-12-02 09:57:50 +0000

    Day 2 part 1 completed

---
+++ day02/part1/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type position struct {
+	horizontal int
+	depth      int
+}
+
+func (p *position) move(n int) {
+	p.horizontal += n
+}
+
+func (p *position) climb(n int) {
+	p.depth -= n
+}
+
+func (p *position) dive(n int) {
+	p.depth += n
+}
+
+func main() {
+	p := position{0, 0}
+
+	input, err := os.Open("input.txt")
+	if err != nil {
+		panic(err)
+	}
+	defer input.Close()
+
+	scanner := bufio.NewScanner(input)
+	for scanner.Scan() {
+		line := strings.Split(scanner.Text(), " ")
+		i := line[0]
+		v, _ := strconv.Atoi(line[1])
+
+		if strings.ToLower(i) == "forward" {
+			p.move(v)
+		}
+		if strings.ToLower(i) == "down" {
+			p.dive(v)
+		}
+		if strings.ToLower(i) == "up" {
+			p.climb(v)
+		}
+	}
+
+	answer := p.horizontal * p.depth
+	fmt.Printf("Answer: %v\n", answer)
+}
