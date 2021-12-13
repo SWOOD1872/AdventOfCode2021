8f8b4640f90e93b353f5f0621a35a89088fb8871
Sam Wood <samwooddev@gmail.com>
2021-12-13 09:46:02 +0000

    Progress

--- day08/part2/main.go
+++ day08/part2/main.go
@@ -49,8 +49,8 @@ func main() {
 
 	for _, rows := range allLines {
 		mapping := make(map[int]string)
-		fmt.Println("Rows:\n", rows)
-		for ic, col := range rows {
+		fmt.Println("Rows:\n", rows[0:10])
+		for ic, col := range rows[0:10] {
 			if ic > 9 {
 				break
 			}
@@ -70,32 +70,37 @@ func main() {
 
 			// Now find the harder numbers
 			if len(col) == 5 {
-				if strings.ContainsAny(col, mapping[1]) && mapHasVal(mapping, col) == false {
-					mapping[2] = col
-					fmt.Printf("%s=%d\n", col, 2)
-				} else if strings.ContainsAny(col, mapping[7]) && mapHasVal(mapping, col) == false {
+				if containsAll(col, mapping[7]) {
 					mapping[3] = col
 					fmt.Printf("%s=%d\n", col, 3)
-				} else {
+					continue
+				} else if containsN(mapping[4], col) == 2 {
+					mapping[2] = col
+					fmt.Printf("%s=%d\n", col, 2)
+					continue
+				} else if containsN(mapping[4], col) == 3 {
 					mapping[5] = col
 					fmt.Printf("%s=%d\n", col, 5)
+					continue
 				}
 			}
 			if len(col) == 6 {
-				if strings.ContainsAny(col, mapping[1]) && mapHasVal(mapping, col) == false {
-					mapping[6] = col
-					fmt.Printf("%s=%d\n", col, 6)
-				} else if strings.ContainsAny(col, mapping[4]) && mapHasVal(mapping, col) == false {
+				if containsAll(col, mapping[1]) && containsAll(col, mapping[3]) && containsAll(col, mapping[4]) && containsAll(col, mapping[5]) {
 					mapping[9] = col
 					fmt.Printf("%s=%d\n", col, 9)
+					continue
+				} else if containsAll(col, mapping[1]) && containsAll(col, mapping[5]) {
+					mapping[6] = col
+					fmt.Printf("%s=%d\n", col, 6)
+					continue
 				} else {
 					mapping[0] = col
 					fmt.Printf("%s=%d\n", col, 0)
+					continue
 				}
 			}
 		}
 		fmt.Println(mapping)
-		break
 	}
 }
 
@@ -107,4 +112,26 @@ func mapHasVal(m map[int]string, s string) bool {
 		}
 	}
 	return false
+}
+
+// containsAll returns true if a given string contains all of a given set of characters
+func containsAll(s, c string) bool {
+	chars := strings.Split(c, "")
+	for _, char := range chars {
+		if !strings.Contains(s, char) {
+			return false
+		}
+	}
+	return true
+}
+
+func containsN(from, in string) int {
+	fromChars := strings.Split(from, "")
+	c := 0
+	for _, v := range fromChars {
+		if strings.Contains(in, v) {
+			c += 1
+		}
+	}
+	return c
 }
